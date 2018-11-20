421180c13f11e67b3087ac64179335e3b1bbe010
mbA2D <ottawaelectronics2016@gmail.com>
2018-11-19 20:20:46 -0500

    Add files via upload

---
+++ initialization.c
@@ -0,0 +1,93 @@
+#ifndef INTIALIZATION
+#define INTIALIZATION
+
+//Constants
+const int MAXNUMTARGETS = 8;
+//Avg firing distance of robot
+const float RANGE = 20;
+const int SPEED = 50;
+const int NUMBALLS = 8;
+const int FIRESPEED = -30;
+const float GEARRADIUS = 2.75;
+const float GOOSETOL = 20;
+const float TOL = 1*pow(10,-20);
+
+//Naming Motors
+const tMotor leftMotor = motorA;
+const tMotor rightMotor = motorD;
+const tMotor hopperMotor = motorB;
+const tMotor firingMotor = motorC;
+
+//Naming Sensors
+const tSensors gyro = S2;
+const tSensors ultraSonic = S3;
+const tSensors touch = S4;
+
+//Display Sizes
+const int WIDTH = 178;
+const int HEIGHT = 128;
+
+// Structs
+typedef struct
+{
+	float x;
+	float y;
+}Coordinate;
+
+typedef struct
+{
+	Coordinate locations[MAXNUMTARGETS];
+}locArr;
+
+typedef struct
+{
+	Coordinate location;
+	float angle;
+}Tank;
+
+//Setup Sensors
+//sets up the touch, ultrasonic, and gyro sensors
+void setupSensors()
+{
+	SensorType[touch] = sensorEV3_Touch;
+	SensorType[ultraSonic] = sensorEV3_Ultrasonic;
+	SensorType[gyro] = sensorEV3_Gyro;
+	wait1Msec(50);
+	SensorMode[gyro] = modeEV3Gyro_RateAndAngle;
+	wait1Msec(50);
+	//wait for robot to be still in order to reset the gyro
+	//while(getGyroRate(gyro) != 0)
+	//{}
+	//resetGyro(gyro);
+}
+
+void setupLocArrStruct(locArr & setupStruct)
+{
+	for(int index = 0; index < MAXNUMTARGETS; index++)
+	{
+		setupStruct.locations[index].x = 0;
+		setupStruct.locations[index].y = 0;
+	}
+}
+
+TEV3Buttons getButtonPressed()
+{
+    TEV3Buttons buttonPressed = buttonNone;
+    while(!getButtonPress(buttonAny))
+    {} //wait for a button to be pressed
+    if(getButtonPress(buttonUp))
+        buttonPressed = buttonUp;
+    else if (getButtonPress(buttonDown))
+        buttonPressed = buttonDown;
+    else if (getButtonPress(buttonLeft))
+        buttonPressed = buttonLeft;
+    else if (getButtonPress(buttonRight))
+        buttonPressed = buttonRight;
+    else if (getButtonPress(buttonEnter))
+        buttonPressed = buttonEnter;
+    while (getButtonPress(buttonAny))
+    {} //wait for button release
+    return buttonPressed;
+}
+
+#endif
---
+++ inputCoordinates.c
@@ -0,0 +1,96 @@
+//////CANNOT RETURN A POINT!!!
+//// MUST PASS BY REFERENCE AN EXISTING COORDINATE VAR
+//^Fixed
+
+#include "initialization.c"
+
+#ifndef INPUT_COORDINATES
+#define INPUT_COORDINATES
+
+
+//creates an on-screen menu to select a single point to fire at.
+void getCoordinates(Coordinate coord)
+{
+	//Assume robot starts facing the X+ direction.
+	//Left and Right arrows to increment the y value, up down increment the x value
+	//EnterButton saves the point.
+
+	int xLoc = 0, yLoc = 0;
+
+	eraseDisplay();
+	drawLine(0, HEIGHT/2, WIDTH, HEIGHT/2);//horizontal line
+	drawLine(WIDTH/2,0,WIDTH/2+1,HEIGHT);//vertical line
+	displayStringAt(0,8,"X=%d  ",xLoc);
+	displayStringAt(0,16,"Y=%d  ", yLoc);
+
+	TEV3Buttons buttonPressed = buttonNone;
+
+	do
+	{
+		buttonPressed = getButtonPressed();
+
+		clearPixel(xLoc+WIDTH/2,yLoc+HEIGHT/2);
+
+		if(buttonPressed == buttonRight)
+			yLoc--;
+		else if (buttonPressed == buttonLeft)
+			yLoc++;
+		else if (buttonPressed == buttonUp)
+			xLoc++;
+		else if (buttonPressed == buttonDown)
+			xLoc--;
+		if(buttonPressed != buttonEnter)
+		{
+			setPixel(xLoc+WIDTH/2,yLoc+HEIGHT/2);
+			displayStringAt(0,8,"X=%d  ",xLoc);
+			displayStringAt(0,16,"Y=%d  ",yLoc);
+		}
+	}while (buttonPressed != buttonEnter);
+
+	coord.x = xLoc;
+	coord.y = yLoc;
+}
+
+int getMultipleCoordinates(locArr & targetLocations)
+{
+	int numTargets = 0;
+	TEV3Buttons buttonPressed = buttonEnter;
+
+	while(buttonPressed == buttonEnter)
+	{
+		eraseDisplay();
+		displayCenteredBigTextLine(3,"Enter Target?");
+		displayCenteredTextLine(6,"Center = YES  Other = NO");
+		//vertical line offset by 1 so that it is visible on the screen
+		//^for some weird reason
+		buttonPressed = getButtonPressed();
+		if(buttonPressed == buttonEnter)
+		{
+			getCoordinates(targetLocations.locations[numTargets]);
+			numTargets++;
+		}
+	}
+
+	eraseDisplay();
+	return numTargets;
+}
+
+void showDestinations(locArr const & destinations)
+{
+	eraseDisplay();
+	for(int i = 0; i < MAXNUMTARGETS; i++)
+	displayTextLine(i+1, "%f  %f", destinations.locations[i].x, destinations.locations[i].y);
+}
+
+//testing main for these functions - All work!
+/*
+task main()
+{
+	locArr destinations;
+	setupLocArrStruct(destinations);//initializes all values to 0,0
+	getMultipleCoordinates(destinations);
+	showDestinations(destinations);
+	wait1Msec(5000);
+}
+*/
+#endif
