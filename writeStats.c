a9cc8d10dd884b38b08f1c351b25a1a7670a533f
lukaswormald1 <lukaswormald1@gmail.com>
2018-11-24 09:18:36 -0500

    Update writeStats.c

--- writeStats.c
+++ writeStats.c
@@ -1,13 +1,19 @@
+// commented by Lukas
+
 #include "PC_FileIO.c"
 #ifndef WRITE_STATS
 #define WRITE_STATS
-
+/*
+Function to output stats to a file:
+Function will output total run time of mission, the number of geese shot, the mission status (success or failure),
+the amount of ammo remaining, and the total number of targets
+*/
 void writeStatsToFile(float runTime, int numGeeseShot, string & missionStatus, int ammoRemaining, int totalTargets)
 {
 	TFileHandle fout;
 	bool fileOkay = openWritePC(fout, "stats.txt");
-
-	runTime = time1[T1] / 1000;
+	
+	runTime = time1[T1] / 1000; // converts time to seconds
 	writeLongPC(fout,runTime);
 	writeEndlPC(fout);
 	writeLongPC(fout,numGeeseShot);
