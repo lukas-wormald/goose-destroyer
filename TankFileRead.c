de3fde0031ed57390e9fd04815042a282e9ed89e
mbA2D <ottawaelectronics2016@gmail.com>
2018-11-24 10:04:10 -0500

    Update TankFileRead.c

--- TankFileRead.c
+++ TankFileRead.c
@@ -1,14 +1,19 @@
-// function to read in coordinates from a file - these will all be in cm
+//commenting complete
 #include "PC_FileIO.c"
 #include "initialization.c"
 
 #ifndef TANK_FILE_READ
 #define TANK_FILE_READ
 
-int readCoordinates(locArr locs, TFileHandle fin)
+//reads coordinates in from a file and loads them into the location array
+//returns the number of targets read in
+//assumes that the only things in the file are floats
+int readCoordinates(locArr & targets, TFileHandle & fin)
 {
-	int numTargets = 0; //-1 in MAXNUMTARGETS for some wierd reason??
-	while(readFloatPC(fin, locs.locations[numTargets].x) && readFloatPC(fin, locs.locations[numTargets].y) && numTargets < MAXNUMTARGETS-1)
+	int numTargets = 0;
+	while(readFloatPC(fin, targets.locations[numTargets].x) && 
+	      readFloatPC(fin, targets.locations[numTargets].y) && 
+	      numTargets < MAXNUMTARGETS-1)
 		numTargets++;
 	return numTargets;
 }
