2c4fb80d5f337ce719be1563119f86372cc430e1
mbA2D <ottawaelectronics2016@gmail.com>
2018-11-24 09:22:26 -0500

    Update inputCoordinates.c

--- inputCoordinates.c
+++ inputCoordinates.c
@@ -1,34 +1,33 @@
 //TESTED AND WORKS
+//Commenting complete
 
 #include "initialization.c"
 
 #ifndef INPUT_COORDINATES
 #define INPUT_COORDINATES
 
-
 //creates an on-screen menu to select a single point to fire at.
-void getCoordinates(Coordinate coord)
+void getCoordinates(Coordinate & coord)
 {
 	//Assume robot starts facing the X+ direction.
-	//Left and Right arrows to increment the y value, up down increment the x value
+	//Left and Right arrows to increment the x value, up down increment the y value
 	//EnterButton saves the point.
 
 	int xLoc = 0, yLoc = 0;
-
+	
+	//displays a grid and and X Y locations on the screen
 	eraseDisplay();
 	drawLine(0, HEIGHT/2, WIDTH, HEIGHT/2);//horizontal line
 	drawLine(WIDTH/2,0,WIDTH/2+1,HEIGHT);//vertical line offset by 1 to make it visible, otherwise does not show
 	displayStringAt(0,8,"X=%d  ",xLoc);
 	displayStringAt(0,16,"Y=%d  ", yLoc);
 
+	//Increment locations with button pushes, and move a pixels
 	TEV3Buttons buttonPressed = buttonNone;
-
 	do
 	{
 		buttonPressed = getButtonPressed();
-
 		clearPixel(xLoc+WIDTH/2,yLoc+HEIGHT/2);
-
 		if(buttonPressed == buttonRight)
 			xLoc+=10;
 		else if (buttonPressed == buttonLeft)
@@ -49,6 +48,8 @@ void getCoordinates(Coordinate coord)
 	coord.y = yLoc;
 }
 
+//fills an array of locations with locations selected by the above function
+//returns the number of targets entered
 int getMultipleCoordinates(locArr & targetLocations)
 {
 	int numTargets = 0;
@@ -72,12 +73,14 @@ int getMultipleCoordinates(locArr & targetLocations)
 	return numTargets;
 }
 
+//Displays the locations selected by the on screen menu in order to verify the entries, used for testing only
 void showDestinations(locArr const & destinations)
 {
 	eraseDisplay();
-	for(int i = 0; i < MAXNUMTARGETS; i++)
-	displayTextLine(i+1, "%f  %f", destinations.locations[i].x, destinations.locations[i].y);
+	for(int index = 0; index < MAXNUMTARGETS; index++)
+	displayTextLine(index+1, "%f  %f", destinations.locations[index].x, destinations.locations[index].y);
 }
+
 
 //testing main for these functions - All work!
 /*
