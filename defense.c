e46641df3734f5934beb596757e18bcd782e94b1
mbA2D <ottawaelectronics2016@gmail.com>
2018-11-24 09:27:38 -0500

    Update defense.c

--- defense.c
+++ defense.c
@@ -1,3 +1,4 @@
+//commenting complete
 #include "initialization.c"
 #include "fire.c"
 #include "tankMovements.c"
@@ -5,10 +6,13 @@
 #ifndef DEFENSE
 #define DEFENSE
 
+//spins the around in a circle, firing off all of the remaining ammo.
+//This is run when the goose has been attacked.
+//This function was tested by 'attacking' the tank and verifying proper behaviour
 void defend (Tank tank0, Stats mission)
 {
 	float turn = 2 * 3.14159 / mission.ammoRemaining;
-	bool isAttackedExtra = true; //this will only run if it has been attacked, but we still need a bool to pass to the firing function
+	bool isAttackedExtra = true; //this function is only run if the robot has been attacked, but we still need a bool to pass to the firing function
 	for (int shots = 0; shots < mission.ammoRemaining; shots ++)
 	{
 		turnAngle(tank0, turn + tank0.angle, isAttackedExtra);
