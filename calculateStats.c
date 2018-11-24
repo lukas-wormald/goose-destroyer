32e962b4cca77a9c5d28bfa3abdec0beecd6fa94
CalebDueck <44909109+CalebDueck@users.noreply.github.com>
2018-11-24 09:40:04 -0500

    Update calculateStats.c

--- calculateStats.c
+++ calculateStats.c
@@ -1,4 +1,3 @@
-// commenting in progress
 #include "PC_FileIO.c"
 #include "initialization.c"
 #ifndef CALCULATE_STATS
@@ -6,40 +5,45 @@
 
 int ammoRemaining = 0;
 
-//void calculateStats(int runTime, string missionStatus, int targetsShot, int totalTargets, bool isAttacked, int outOfRange)
 void calculateStats(Stats mission)
 {
 
 	TFileHandle fout;
 	bool fileOkay = openWritePC(fout, "stats.txt");
-
+	
+	//if there are not NUMBALLS, tank needs reloading
 	mission.needReload = (mission.ammoRemaining < NUMBALLS);
 
-	mission.runTime = time1[T1] / 1000;
+	//how long the mission took
+	mission.runTime = time1[missionTimer] / 1000;
 	writeLongPC(fout,mission.runTime);
 	writeTextPC(fout, " seconds");
 	writeEndlPC(fout);
 
+	//how many geese shot
 	writeLongPC(fout, mission.targetsShot);
 	writeTextPC(fout, " geese shot");
 	writeEndlPC(fout);
 
+	//how many targets are out of range
 	writeTextPC(fout, "Targets out of range: ");
 	writeLongPC(fout, mission.outOfRange);
 	writeEndlPC(fout);
 
-	if (mission.ammoRemaining < 8)
+	//writes if it needs to be reloaded
+	if (mission.needReload)
 		writeTextPC(fout, "Robot needs reloading");
-	else if (mission.ammoRemaining >= 8)
+	else if (mission.needReload)
 		writeTextPC(fout, "Robot does not need reloading");
 	writeEndlPC(fout);
 
+	//Failed the mission
 	if (mission.isAttacked)
-		mission.missionStatus = "Mission was a Failure";
+		mission.missionStatus = "Mission was a Failure, Attacked by Goose";
 	else if(mission.totalTargets - mission.targetsShot == 0)
 		mission.missionStatus = "Mission was a Success!";
 	else
-		mission.missionStatus = "Mission was a Failure";
+		mission.missionStatus = "Mission was a Failure, Not all targets Shot";
 	writeTextPC(fout, mission.missionStatus);
 	writeEndlPC(fout);
 
