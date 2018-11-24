2e7b095fa9e2c632c6fa0dc7451701ad22ffe754
CalebDueck <44909109+CalebDueck@users.noreply.github.com>
2018-11-24 09:14:08 -0500

    Update calculations.c

--- calculations.c
+++ calculations.c
@@ -2,20 +2,27 @@
 #ifndef CALCULATIONS
 #define CALCULATIONS
 //Calculations
-//Distance to closest firing position
 
+
+//Distance to specific point
 float distToCoordinate (Coordinate const & tank0Loc, Coordinate const & target)
 {
+	//Using Pythagoras
 	return sqrt(pow(tank0Loc.x - target.x, 2) + pow(tank0Loc.y - target.y, 2));
 }
 
+
+//Distance to closest firing position
 float distToRange (Coordinate const & tank0Loc, Coordinate const & target)
 {
+	//Using Pythagoras - the range
 	return distToCoordinate(tank0Loc, target) - RANGE;
 }
 
+//Finds if the target is too far away
 bool targetInRange(Tank const tank0, Coordinate const target)
 {
+	//distance div time < max travel time
 	return (distToRange(tank0.location, target) / BOTSPEED < MAXSECONDSRANGE);
 }
 
@@ -24,14 +31,13 @@ float angleBetween (Coordinate const & tank0Loc, Coordinate const & target)
 {
 	float delx = target.x - tank0Loc.x;
 	float dely = target.y - tank0Loc.y;
-	float angle = 0;
 	//range of arctan is -PI/2 to PI/2
 	if (delx<0 && dely>=0)
 		return atan(dely / delx) + PI;
 	else if (abs(delx) < TOL && dely > 0)
 		return PI / 2.0;
 	else if (abs(delx) < TOL && dely < 0)
-		return -(PI / 2.0);
+		return -(PI / 2.0); //For some reason robot C could not understand -PI / 2.0 so it needed brackets
 	else if (delx<0 && dely<0)
 		return atan(dely / delx) - PI;
 	else if (delx>0)
